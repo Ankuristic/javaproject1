e98c0b070070edc7f4f14ff9d8beecdc0a7c8c4e
abc <ankur.sh2011@gmail.com>
2022-05-13 15:01:51 +0530

    123

---
+++ java-oops/src/examples/activity3.java
@@ -0,0 +1,20 @@
+package examples;
+
+
+
+public class activity3 {
+	public class Looping {
+
+		  public static void main(String args[]) {
+		     int x = 10;
+
+		     while( x < 15 ) {
+		        System.out.print("value x : " + x );
+		        x++;
+		        System.out.print("\\n");
+		        
+		     }
+		  }
+		}
+
+}
