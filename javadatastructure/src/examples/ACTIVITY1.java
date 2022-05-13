6ba23d5c37f4313479098ef26a2eeb3ab9f4fec3
abc <ankur.sh2011@gmail.com>
2022-05-13 15:33:35 +0530

    datastructure 3 staging

---
+++ javadatastructure/src/examples/ACTIVITY1.java
@@ -0,0 +1,13 @@
+package examples;
+import logic.*;
+import java.util.*;
+
+public class ACTIVITY1 {
+		public static void main (String args[ ])
+		{
+		for (int i = 0 ; i <= -100 ; i-- )
+		{			
+		System.out.println("Java Developer : "+i);
+		}
+		}
+}
